5e19012c1872d946772f0f7ba412412b21dd2fe8
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2021-11-18 11:07:17 +0530

    Create String Validators.py

---
+++ Python/String Validators.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    s = input()
+    l=[False, False, False, False, False]
+    for c in s:
+        if c.isalnum():
+            l[0]=True
+        if c.isalpha():
+            l[1]=True
+        if c.isdigit():
+            l[2]=True
+        if c.islower():
+            l[3]=True
+        if c.isupper():
+            l[4]=True
+    for i in l:
+        print(i)
