d9f55a6eaca1febcd108efa14fe85eceb141c98d
Dhrati Bajpai <66199432+dhratibaj@users.noreply.github.com>
2021-11-27 21:40:26 +0530

    Create Simple Array Sum.py

---
+++ Problem Solving/Python/Simple Array Sum.py
@@ -0,0 +1,33 @@
+ar_count = int(input().strip())
+ar = list(map(int, input().rstrip().split()))
+s = 0
+for i in ar:
+    s = s + i
+print(s)
+
+#-------------------another approach-----------------------------
+
+ar_count = int(input().strip())
+ar = list(map(int, input().rstrip().split()))
+print(sum(ar))
+
+#--------------------inbuild code---------------------------------
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
